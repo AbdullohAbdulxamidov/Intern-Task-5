66c9af93811c3d5867a03de075e7ea9c0db4af02
Abdulloh <Aabbdduulloohhh@gmail.com>
2025-03-29 15:48:17 +0500

    Fix faker.Faker constructor issue

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ let currentData = [];
 let isFetching = false;
 
 // Backend URL (relative path since backend is hosted on the same Vercel app)
-const backendUrl = "/api/books";
+const backendUrl = "https://intern-task-5.vercel.app/api/books";
 
 document.getElementById('likes').addEventListener('input', (e) => {
     document.getElementById('likesValue').textContent = e.target.value;
